067ee657a56403292d5930870a75b5e26570fa5a
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-09-16 01:40:49 -0700

    first steps to http actions

--- migrations.py
+++ migrations.py
@@ -3,24 +3,28 @@ async def m001_initial(db):
     Initial jobs table.
     """
     await db.execute(
-        """
+    """
         CREATE TABLE scheduler.jobs (
             id TEXT PRIMARY KEY,
             name TEXT NOT NULL,
             admin TEXT NOT NULL,
-            command TEXT,
-            schedule TEXT
+            schedule TEXT,
+            httpverb TEXT,
+            url TEXT,
+            headers JSON,
+            body JSON,
         );
     """
     )
 
 
-async def m002_add_jobs_attrs_column(db):
-    """
-    Initial jobs table.
-    """
-    await db.execute(
-        """
-        ALTER TABLE scheduler.jobs ADD COLUMN extra JSON
-    """
-    )
+# async def m002_add_jobs_attrs_column(db):
+#     """
+#     Initial jobs table.
+#     """
+#     await db.execute(
+#         """
+#         ALTER TABLE scheduler.jobs
+#         ADD COLUMN extra JSON
+#         """
+#     )
