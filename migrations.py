cb61deb01c9b3263222c47a2ee8cd5c844ab4b85
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-09-19 22:48:20 -0700

    fix migrations

--- migrations.py
+++ migrations.py
@@ -4,16 +4,38 @@ async def m001_initial(db):
     Initial jobs table.
     """
     await db.execute(
-    """
+        """
         CREATE TABLE scheduler.jobs (
             id TEXT PRIMARY KEY,
             name TEXT NOT NULL,
             admin TEXT NOT NULL,
-            schedule TEXT,
-            httpverb TEXT,
-            url TEXT,
-            headers JSON,
-            body JSON,
+            command TEXT,
+            schedule TEXT
         );
+    """
+    )
+
+async def m002_add_jobs_attrs_column(db):
+    """
+    Initial jobs table.
+    """
+    await db.execute(
+        """
+        ALTER TABLE scheduler.jobs ADD COLUMN extra JSON
+    """
+    )
+
+async def m003_update_columns_for_api(db):
+    # trunk-ignore(ruff/D401)
+    """
+    Initial jobs table.
+    """
+    await db.execute(
+    """
+        ALTER TABLE scheduler.jobs DROP COLUMN command TEXT;
+        ALTER TABLE scheduler.jobs ADD COLUMN httpverb TEXT;
+        ALTER TABLE scheduler.jobs ADD COLUMN url TEXT;
+        ALTER TABLE scheduler.jobs ADD COLUMN headers JSON;
+        ALTER TABLE scheduler.jobs ADD COLUMN body JSON;
     """
     )
